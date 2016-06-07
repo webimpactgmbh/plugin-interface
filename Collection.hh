bd23e3efbc2146ba2f7bc86a28890fffdd40ca89
plentymonkey <buildmonkey@plentymarkets.com>
2016-06-07 09:04:16 +0000

    NEW build for branch beta7

--- Collection.hh
+++ Collection.hh
@@ -1,7 +1,9 @@
 <?hh
 namespace Illuminate\Database\Eloquent;
 
+use LogicException;
 use Illuminate\Support\Arr;
+use Illuminate\Contracts\Queue\QueueableCollection;
 use Illuminate\Support\Collection as BaseCollection;
 
 class Collection extends Illuminate\Support\Collection 
