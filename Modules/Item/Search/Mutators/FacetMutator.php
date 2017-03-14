2d2ced82507c77515e825df68635f32de4a4d74b
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-03-14 09:18:30 +0100

    Automated interface update 2017-03-14_09:18:30

---
+++ Modules/Item/Search/Mutators/FacetMutator.php
@@ -0,0 +1,21 @@
+<?php
+namespace Plenty\Modules\Item\Search\Mutators;
+
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\Mutator\BaseMutator;
+
+abstract class FacetMutator 
+{
+
+	abstract public function mutate(
+		array $data
+	):array;
+
+	abstract public static function facetSort(
+		array $a, 
+		array $b
+	):int;
+
+	abstract public function getDependencies(
+	):array;
+
+}
