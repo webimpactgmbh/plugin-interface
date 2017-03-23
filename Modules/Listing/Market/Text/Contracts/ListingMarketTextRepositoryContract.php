b21df465988e0f5ca84169d59279ccfe73d491b5
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-03-23 09:18:25 +0100

    Automated interface update 2017-03-23_09:18:25

---
+++ Modules/Listing/Market/Text/Contracts/ListingMarketTextRepositoryContract.php
@@ -0,0 +1,79 @@
+<?php
+namespace Plenty\Modules\Listing\Market\Text\Contracts;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Plenty\Exceptions\ModelWithUniqueFieldException;
+use Plenty\Modules\Listing\Market\Text\Models\ListingMarketText;
+use Plenty\Repositories\Contracts\FilterableContract;
+use Plenty\Repositories\Criteria\Contracts\CriteriableContract;
+use Plenty\Repositories\Models\PaginatedResult;
+
+/**
+ * The contract for the listing market text repository.
+ */
+interface ListingMarketTextRepositoryContract 
+{
+
+	/**
+	 * Get a listing market text
+	 */
+	public function get(
+		int $id
+	):ListingMarketText;
+
+	/**
+	 * Create new listing market text
+	 */
+	public function create(
+		int $listingMarketId, 
+		array $data
+	):ListingMarketText;
+
+	/**
+	 * Update listing market text
+	 */
+	public function update(
+		int $listingMarketId, 
+		string $lang, 
+		array $data
+	):ListingMarketText;
+
+	/**
+	 * Update or create listing market text
+	 */
+	public function updateOrCreate(
+		int $listingMarketId, 
+		string $lang, 
+		array $data
+	):ListingMarketText;
+
+	/**
+	 * Delete listing market text
+	 */
+	public function delete(
+		int $id
+	):bool;
+
+	/**
+	 * List listing market text
+	 */
+	public function search(
+		int $page = 1, 
+		int $itemsPerPage = 50, 
+		array $filters = []
+	):array;
+
+	/**
+	 * Sets the filter array.
+	 */
+	public function setFilters(
+		array $filters = []
+	);
+
+	/**
+	 * Returns the filter array.
+	 */
+	public function getFilters(
+	);
+
+}
---
+++ Modules/Listing/Market/Text/Models/ListingMarketText.php
@@ -0,0 +1,32 @@
+<?php
+namespace Plenty\Modules\Listing\Market\Text\Models;
+
+
+/**
+ * The listing market text model.
+ */
+abstract class ListingMarketText 
+{
+
+	const MAX_ITEMS_PER_PAGE = 50;
+
+	const CREATED_AT = 'createdAt';
+
+	const UPDATED_AT = 'updatedAt';
+	public		$id;
+	public		$listingMarketId;
+	public		$lang;
+	public		$title;
+	public		$subtitle;
+	public		$description;
+	
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
