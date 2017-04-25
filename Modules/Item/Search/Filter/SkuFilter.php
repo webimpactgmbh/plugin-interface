927b8397f42865eea911279cd10e849c07d90798
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-04-25 07:28:31 +0200

    Automated interface update 2017-04-25_07:28:31

---
+++ Modules/Item/Search/Filter/SkuFilter.php
@@ -0,0 +1,34 @@
+<?php
+namespace Plenty\Modules\Item\Search\Filter;
+
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Statement\Filter\TermFilter;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Statement\StatementInterface;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Type\Filter\BoolMustNestedFilter;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Type\TypeInterface;
+
+/**
+ * foo
+ */
+abstract class SkuFilter implements TypeInterface
+
+{
+
+	abstract public function getPath(
+	);
+
+	abstract public function hasMarketId(
+		float $marketId
+	);
+
+	abstract public function hasAccountId(
+		int $accountId
+	);
+
+	abstract public function toArray(
+	):array;
+
+	abstract public function addStatement(
+		StatementInterface $statement
+	);
+
+}
