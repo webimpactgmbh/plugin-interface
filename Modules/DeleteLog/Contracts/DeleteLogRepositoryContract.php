d1d39d931372bc965af1933c33b01e1285df1769
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-05-03 16:58:42 +0200

    Automated interface update 2017-05-03_16:58:42

---
+++ Modules/DeleteLog/Contracts/DeleteLogRepositoryContract.php
@@ -0,0 +1,35 @@
+<?php
+namespace Plenty\Modules\DeleteLog\Contracts;
+
+use Plenty\Repositories\Contracts\FilterableContract;
+use Plenty\Repositories\Criteria\Contracts\CriteriableContract;
+use Plenty\Repositories\Models\PaginatedResult;
+
+/**
+ * List delete log entries.
+ */
+interface DeleteLogRepositoryContract 
+{
+
+	/**
+	 * List delete log entries
+	 */
+	public function search(
+		int $page, 
+		int $itemsPerPage
+	):PaginatedResult;
+
+	/**
+	 * Sets the filter array.
+	 */
+	public function setFilters(
+		array $filters = []
+	);
+
+	/**
+	 * Returns the filter array.
+	 */
+	public function getFilters(
+	);
+
+}
---
+++ Modules/DeleteLog/Models/DeleteLog.php
@@ -0,0 +1,55 @@
+<?php
+namespace Plenty\Modules\DeleteLog\Models;
+
+
+/**
+ * The delete log model
+ */
+abstract class DeleteLog 
+{
+
+	const REFERENCE_TYPE_CATEGORY = 1;
+
+	const REFERENCE_TYPE_PAYMENTDATA = 2;
+
+	const REFERENCE_TYPE_ORDERDATA = 3;
+
+	const REFERENCE_TYPE_CUSTOMER = 4;
+
+	const REFERENCE_TYPE_ITEM = 5;
+
+	const REFERENCE_TYPE_FACET_ATTRIBUTE = 6;
+
+	const REFERENCE_TYPE_FACET_CHARACTER = 7;
+
+	const REFERENCE_TYPE_FACET_PRODUCER = 8;
+
+	const REFERENCE_TYPE_FACET_AVAILIBILITY = 9;
+
+	const REFERENCE_TYPE_UNIT = 10;
+
+	const REFERENCE_TYPE_RETAIL_PRICE = 11;
+
+	const REFERENCE_TYPE_RETAIL_PRICE_LINK = 12;
+
+	const REFERENCE_TYPE_ITEM_VARIATION = 13;
+
+	const UPDATED_AT = 'updatedAt';
+
+	const CREATED_AT = 'createdAt';
+	public		$referenceType;
+	public		$referenceValue;
+	public		$userRealName;
+	public		$updatedAt;
+	public		$userId;
+	
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
