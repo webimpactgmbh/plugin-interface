a597dc0aa700b72ddac52cc03c03321d170233ff
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-05-04 12:56:52 +0200

    Automated interface update 2017-05-04_12:56:52

---
+++ Modules/Payment/MethodName/Models/PaymentMethodName.php
@@ -0,0 +1,27 @@
+<?php
+namespace Plenty\Modules\Payment\MethodName\Models;
+
+
+/**
+ * The payment method name model
+ */
+abstract class PaymentMethodName 
+{
+
+	const CREATED_AT = 'createdAt';
+
+	const UPDATED_AT = 'updatedAt';
+	public		$paymentMethodId;
+	public		$lang;
+	public		$name;
+	
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
