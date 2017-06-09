abf1bad1af1826f404bb1a2111a2f6cb1e740a9e
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-06-09 09:32:51 +0200

    Automated interface update 2017-06-09_09:32:51

---
+++ Modules/System/Contracts/WebstoreConfigurationRepositoryContract.php
@@ -0,0 +1,20 @@
+<?php
+namespace Plenty\Modules\System\Contracts;
+
+use Plenty\Modules\System\Models\WebstoreConfiguration;
+
+/**
+ * Repository for Webstore Configuration
+ */
+interface WebstoreConfigurationRepositoryContract 
+{
+
+	public function findByPlentyId(
+		int $plentyId
+	):WebstoreConfiguration;
+
+	public function findByWebstoreId(
+		int $webstoreId
+	):WebstoreConfiguration;
+
+}
