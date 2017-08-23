dc55c484db27b7c41321c5220896ef8ef1a327ba
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-08-23 12:10:25 +0200

    Automated interface update 2017-08-23_12:10:25

---
+++ Modules/Frontend/Models/TotalVat.php
@@ -0,0 +1,23 @@
+<?php
+namespace Plenty\Modules\Frontend\Models;
+
+
+/**
+ * frontend total vat model
+ */
+abstract class TotalVat 
+{
+	public		$vatId;
+	public		$vatAmount;
+	public		$vatValue;
+	
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
--- Modules/Frontend/Services/VatService.php
+++ Modules/Frontend/Services/VatService.php
@@ -2,13 +2,18 @@
 namespace Plenty\Modules\Frontend\Services;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Plenty\Legacy\Services\Checkout\VatServicePartial;
+use Plenty\Legacy\Services\Order\Tax\SessionTaxInformation;
 use Plenty\Modules\Account\Address\Contracts\AddressRepositoryContract;
 use Plenty\Modules\Account\Address\Models\Address;
 use Plenty\Modules\Accounting\Facades\AccountingService;
 use Plenty\Modules\Accounting\Vat\Contracts\VatInitContract;
 use Plenty\Modules\Accounting\Vat\Contracts\VatRepositoryContract;
+use Plenty\Modules\Accounting\Vat\Facades\VatInit;
 use Plenty\Modules\Accounting\Vat\Models\Vat;
+use Plenty\Modules\Frontend\Models\TotalVat;
 use Plenty\Modules\Frontend\Session\Storage\Models\Order;
+use Plenty\Modules\Order\Contracts\PriceCalculatorContract;
 use Plenty\Modules\System\Contracts\WebstoreConfigurationRepositoryContract;
 
 /**
@@ -18,7 +23,10 @@ abstract class VatService
 {
 
 	abstract public function getCountryVatId(
-	);
+	):int;
+
+	abstract public function getCurrentTotalVats(
+	):array;
 
 	abstract public function getVat(
 		string $taxIdNumber = ""
