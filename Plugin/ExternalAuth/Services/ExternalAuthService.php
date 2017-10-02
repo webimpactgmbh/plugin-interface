2176212ae9604b746f7b010ff337a2a9339cd6e1
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-10-02 11:21:23 +0200

    Automated interface update 2017-10-02_11:21:23

--- Plugin/ExternalAuth/Services/ExternalAuthService.php
+++ Plugin/ExternalAuth/Services/ExternalAuthService.php
@@ -1,6 +1,7 @@
 <?php
 namespace Plenty\Plugin\ExternalAuth\Services;
 
+use Plenty\Modules\Authentication\Services\AccountAuthenticationProxy;
 use Plenty\Plugin\ExternalAuth\Contracts\ExternalAccessRepositoryContract;
 use Plenty\Plugin\ExternalAuth\Models\ExternalAccess;
 use Plenty\Plugin\ExternalAuth\Repositories\ExternalAccessRepository;
