c61152003452695c727ff46d1cc54e0fb96fe066
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-10-23 10:53:28 +0200

    Automated interface update 2017-10-23_10:53:28

--- Modules/Listing/Market/Directory/Contracts/ListingMarketDirectoryRepositoryContract.php
+++ Modules/Listing/Market/Directory/Contracts/ListingMarketDirectoryRepositoryContract.php
@@ -4,6 +4,9 @@ namespace Plenty\Modules\Listing\Market\Directory\Contracts;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Plenty\Exceptions\ValidationException;
 use Plenty\Modules\Listing\Market\Directory\Models\ListingMarketDirectory;
+use Plenty\Repositories\Contracts\FilterableContract;
+use Plenty\Repositories\Criteria\Contracts\CriteriableContract;
+use Plenty\Repositories\Criteria\Criteria;
 
 /**
  * The contract for the listing market directory repository.
@@ -15,8 +18,17 @@ interface ListingMarketDirectoryRepositoryContract
 	 * Get a listing market directory
 	 */
 	public function get(
-		int $id
+		int $id, 
+		array $with = []
 	):ListingMarketDirectory;
+
+	/**
+	 * List all directories
+	 */
+	public function all(
+		array $with = [], 
+		array $filters = []
+	):array;
 
 	/**
 	 * Create a listing market directory.
@@ -38,6 +50,43 @@ interface ListingMarketDirectoryRepositoryContract
 	 */
 	public function delete(
 		int $id
+	);
+
+	/**
+	 * Resets all Criteria filters by creating a new instance of the builder object.
+	 */
+	public function clearCriteria(
+	);
+
+	/**
+	 * Applies criteria classes to the current repository.
+	 */
+	public function applyCriteriaFromFilters(
+	);
+
+	/**
+	 * Sets the filter array.
+	 */
+	public function setFilters(
+		array $filters = []
+	);
+
+	/**
+	 * Returns the filter array.
+	 */
+	public function getFilters(
+	);
+
+	/**
+	 * Returns a collection of parsed filters as Condition object
+	 */
+	public function getConditions(
+	);
+
+	/**
+	 * Clears the filter array.
+	 */
+	public function clearFilters(
 	);
 
 }
