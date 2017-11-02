9de90558de5c937e8031ce7bf4b9b54a58812b87
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-11-02 13:22:16 +0100

    Automated interface update 2017-11-02_13:22:16

---
+++ Modules/Item/Search/Aggregations/FeedbackAggregation.php
@@ -0,0 +1,48 @@
+<?php
+namespace Plenty\Modules\Item\Search\Aggregations;
+
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Search\Aggregation\AggregationInterface;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Search\Aggregation\Object\ObjectRangeAggregation;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\SourceInterface;
+
+/**
+ * To be written
+ */
+abstract class FeedbackAggregation implements AggregationInterface
+
+{
+
+	const NAME = 'feedback';
+
+	abstract public function getName(
+	):string;
+
+	abstract public function getField(
+	):string;
+
+	abstract public function getRanges(
+	);
+
+	abstract public function getAggregation(
+	);
+
+	abstract public function toArray(
+	):array;
+
+	abstract public function process(
+		array $data
+	):array;
+
+	abstract public function addSource(
+		SourceInterface $source
+	):AggregationInterface;
+
+	abstract public function setPage(
+		int $page, 
+		int $rowsPerPage
+	):AggregationInterface;
+
+	abstract public function getSources(
+	);
+
+}
