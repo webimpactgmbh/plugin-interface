d39bee2d045319e873e8578fc5428144472219c5
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-11-09 14:22:56 +0100

    Automated interface update 2017-11-09_14:22:56

---
+++ Modules/Item/Attribute/Contracts/AttributeValueSetRepositoryContract.php
@@ -0,0 +1,59 @@
+<?php
+namespace Plenty\Modules\Item\Attribute\Contracts;
+
+use Illuminate\Support\Collection;
+use Plenty\Repositories\Contracts\FilterableContract;
+use Plenty\Repositories\Criteria\Contracts\CriteriableContract;
+use Plenty\Repositories\Criteria\Criteria;
+
+/**
+ * The contract for the attribute value set repository
+ */
+interface AttributeValueSetRepositoryContract 
+{
+
+	/**
+	 * List attribute value sets
+	 */
+	public function search(
+		array $filters = []
+	):Collection;
+
+	/**
+	 * Resets all Criteria filters by creating a new instance of the builder object.
+	 */
+	public function clearCriteria(
+	);
+
+	/**
+	 * Applies criteria classes to the current repository.
+	 */
+	public function applyCriteriaFromFilters(
+	);
+
+	/**
+	 * Sets the filter array.
+	 */
+	public function setFilters(
+		array $filters = []
+	);
+
+	/**
+	 * Returns the filter array.
+	 */
+	public function getFilters(
+	);
+
+	/**
+	 * Returns a collection of parsed filters as Condition object
+	 */
+	public function getConditions(
+	);
+
+	/**
+	 * Clears the filter array.
+	 */
+	public function clearFilters(
+	);
+
+}
