3b1efe2e03e6d16fcefa60e9c041130bd49c5014
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-12-19 14:40:16 +0100

    Automated interface update 2017-12-19_14:40:16

---
+++ Modules/Account/Contact/Contracts/ContactGroupFunctionsRepositoryContract.php
@@ -0,0 +1,21 @@
+<?php
+namespace Plenty\Modules\Account\Contact\Contracts;
+
+
+/**
+ * The ContactGroupFunctionsRepositoryContract is the interface for the contact account group function repository. This interface allows to execute a group function call and get group function related data.
+ */
+interface ContactGroupFunctionsRepositoryContract 
+{
+
+	public function loadGroupFunctions(
+	):array;
+
+	public function executeGroupFunction(
+		array $contactList = [], 
+		int $addressLabelTemplate = null, 
+		int $emailTemplate = null, 
+		int $newsletter = null
+	):array;
+
+}
