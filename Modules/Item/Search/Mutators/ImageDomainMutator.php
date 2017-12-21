b662578d67c34c65d087150963844184a6653801
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-12-21 15:16:27 +0100

    Automated interface update 2017-12-21_15:16:27

---
+++ Modules/Item/Search/Mutators/ImageDomainMutator.php
@@ -0,0 +1,34 @@
+<?php
+namespace Plenty\Modules\Item\Search\Mutators;
+
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\Mutator\MutatorInterface;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\Mutator\ObjectMutator;
+use Plenty\Modules\System\Contracts\WebstoreConfigurationRepositoryContract;
+use Plenty\Modules\System\Models\WebstoreConfiguration;
+
+/**
+ * Converts image domains to client specific image domains.
+ */
+abstract class ImageDomainMutator implements MutatorInterface
+
+{
+
+	abstract public function getPath(
+	):string;
+
+	abstract public function mutateObject(
+		array $data
+	):array;
+
+	abstract public function setClient(
+		int $plentyId
+	):self;
+
+	abstract public function getDependencies(
+	):array;
+
+	abstract public function mutate(
+		array $data
+	):array;
+
+}
