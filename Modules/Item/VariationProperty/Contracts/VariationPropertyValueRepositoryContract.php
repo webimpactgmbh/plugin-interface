10280a9a1d10ab0c3e4c4a5033517bdb11a564bc
BuildMonkey <buildmonkey@plentymarkets.eu>
2018-01-02 12:23:10 +0100

    Automated interface update 2018-01-02_12:23:10

--- Modules/Item/VariationProperty/Contracts/VariationPropertyValueRepositoryContract.php
+++ Modules/Item/VariationProperty/Contracts/VariationPropertyValueRepositoryContract.php
@@ -2,6 +2,7 @@
 namespace Plenty\Modules\Item\VariationProperty\Contracts;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Collection;
 use Plenty\Modules\Item\VariationProperty\Models\VariationPropertyValue;
 use Plenty\Repositories\Models\DeleteResponse;
 
@@ -44,10 +45,31 @@ interface VariationPropertyValueRepositoryContract
 	):DeleteResponse;
 
 	/**
+	 * Deletes all links between a variation and its property values. The ID of the variation must be specified.
+	 */
+	public function deleteAll(
+		int $variationId
+	):DeleteResponse;
+
+	/**
 	 * Lists the property values linked to a variation. The ID of the variation must be specified.
 	 */
 	public function findByVariationId(
 		int $variationId
 	):array;
+
+	/**
+	 * Creates a list of variation properties.
+	 */
+	public function createBulk(
+		array $data
+	):Collection;
+
+	/**
+	 * Updates a list of variation properties.
+	 */
+	public function updateBulk(
+		array $data
+	):Collection;
 
 }
