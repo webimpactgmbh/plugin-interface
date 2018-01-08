93bf032ce59a99e55fa77129c214d15e99f50ff5
BuildMonkey <buildmonkey@plentymarkets.eu>
2018-01-08 02:36:56 +0100

    Automated interface update 2018-01-08_02:36:56

--- Modules/Frontend/Services/FileService.php
+++ Modules/Frontend/Services/FileService.php
@@ -60,4 +60,31 @@ abstract class FileService
 	abstract public function jsonSerialize(
 	);
 
+	/**
+	 * Fill the model with an array of attributes.
+	 */
+	abstract public function fill(
+		array $attributes
+	):self;
+
+	/**
+	 * Determine if the given attribute may be mass assigned.
+	 */
+	abstract public function isFillable(
+		string $key
+	):bool;
+
+	/**
+	 * Get the fillable attributes for the model.
+	 */
+	abstract public function getFillable(
+	):array;
+
+	/**
+	 * Set the fillable attributes for the model.
+	 */
+	abstract public function fillable(
+		array $fillable
+	):self;
+
 }
