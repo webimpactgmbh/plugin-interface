4f8f22e1e6c33f98d63daa81babaf3de0288b033
BuildMonkey <buildmonkey@plentymarkets.eu>
2018-01-26 09:39:11 +0100

    Automated interface update 2018-01-26_09:39:11

---
+++ Modules/Item/Search/Aggregations/ItemCardinalityAggregationProcessor.php
@@ -0,0 +1,24 @@
+<?php
+namespace Plenty\Modules\Item\Search\Aggregations;
+
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Processor\BaseProcessor;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\Mutator\MutatorInterface;
+
+/**
+ * To be written
+ */
+abstract class ItemCardinalityAggregationProcessor 
+{
+
+	abstract public function process(
+		 $data
+	);
+
+	abstract public function getDependencies(
+	):array;
+
+	abstract public function addMutator(
+		MutatorInterface $mutator
+	):self;
+
+}
