a79421984df1269aa45ee984243530a57a3d3c26
BuildMonkey <buildmonkey@plentymarkets.eu>
2018-02-13 17:45:01 +0100

    Automated interface update 2018-02-13_17:45:01

---
+++ Repositories/Criteria/Contracts/ExtendsCriteriaContract.php
@@ -0,0 +1,13 @@
+<?php
+namespace Plenty\Repositories\Criteria\Contracts;
+
+use Illuminate\Database\Eloquent\Builder;
+use Plenty\Repositories\Criteria\Criteria;
+
+/**
+ * Interface which provides the possibility to extends repositories with new Criteria filters.
+ */
+interface ExtendsCriteriaContract 
+{
+
+}
