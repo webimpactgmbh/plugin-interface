39c20b43567bac6ec58d18d51204144eb3a0e5f8
BuildMonkey <buildmonkey@plentymarkets.eu>
2018-02-19 09:51:05 +0100

    Automated interface update 2018-02-19_09:51:05

---
+++ Modules/Plugin/DataBase/Annotations/Relation.php
@@ -0,0 +1,17 @@
+<?php
+namespace Plenty\Modules\Plugin\DataBase\Annotations;
+
+
+abstract class Relation 
+{
+
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
