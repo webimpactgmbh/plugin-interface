e30e3d19e0708df84dc0b6481ab678bb42dcbaf1
BuildMonkey <buildmonkey@plentymarkets.eu>
2018-03-28 11:15:20 +0200

    Automated interface update 2018-03-28_11:15:20

---
+++ Modules/Item/Search/Mutators/VariationPropertyGroupMutator.php
@@ -0,0 +1,21 @@
+<?php
+namespace Plenty\Modules\Item\Search\Mutators;
+
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\Mutator\BaseMutator;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\Mutator\MutatorInterface;
+
+/**
+ * To be written...
+ */
+abstract class VariationPropertyGroupMutator implements MutatorInterface
+
+{
+
+	abstract public function mutate(
+		 $data
+	);
+
+	abstract public function getDependencies(
+	);
+
+}
