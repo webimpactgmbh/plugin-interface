412b278e41f2edcd7437d93134fc8bdc14ebcff6
BuildMonkey <buildmonkey@plentymarkets.eu>
2018-04-12 15:31:15 +0200

    Automated interface update 2018-04-12_15:31:15

--- Modules/Item/VariationMarket/Contracts/VariationMarketRepositoryContract.php
+++ Modules/Item/VariationMarket/Contracts/VariationMarketRepositoryContract.php
@@ -1,9 +1,14 @@
 <?php
 namespace Plenty\Modules\Item\VariationMarket\Contracts;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Plenty\Modules\Item\VariationMarket\Models\VariationMarket;
+use Plenty\Repositories\Contracts\FilterableContract;
+use Plenty\Repositories\Criteria\Contracts\CriteriableContract;
+use Plenty\Repositories\Criteria\Criteria;
 use Plenty\Repositories\Models\DeleteResponse;
+use Plenty\Repositories\Models\PaginatedResult;
 
 /**
  * The contract of the variation market repository
@@ -47,5 +52,65 @@ interface VariationMarketRepositoryContract
 	public function findByVariationIdWithInheritance(
 		int $variationId
 	):VariationMarket;
+
+	/**
+	 * Lists all links between variations and markets.
+	 */
+	public function getVariationMarkets(
+		int $itemsPerPage = 50, 
+		int $page = 1
+	):PaginatedResult;
+
+	/**
+	 * Creates up to 50 links between variations and markets.
+	 */
+	public function createBulk(
+		array $data
+	);
+
+	/**
+	 * Deletes all market links of the variation specified.
+	 */
+	public function deleteBulk(
+		int $itemId, 
+		int $variationId
+	);
+
+	/**
+	 * Sets the filter array.
+	 */
+	public function setFilters(
+		array $filters = []
+	);
+
+	/**
+	 * Returns the filter array.
+	 */
+	public function getFilters(
+	);
+
+	/**
+	 * Returns a collection of parsed filters as Condition object
+	 */
+	public function getConditions(
+	);
+
+	/**
+	 * Clears the filter array.
+	 */
+	public function clearFilters(
+	);
+
+	/**
+	 * Resets all Criteria filters by creating a new instance of the builder object.
+	 */
+	public function clearCriteria(
+	);
+
+	/**
+	 * Applies criteria classes to the current repository.
+	 */
+	public function applyCriteriaFromFilters(
+	);
 
 }
