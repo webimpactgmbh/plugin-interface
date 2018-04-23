ab32fdb513e70a95f7cb05f08cd0f2c3f9cc2571
BuildMonkey <buildmonkey@plentymarkets.eu>
2018-04-23 11:23:43 +0200

    Automated interface update 2018-04-23_11:23:43

--- Modules/Item/Variation/Models/Variation.php
+++ Modules/Item/Variation/Models/Variation.php
@@ -76,6 +76,7 @@ abstract class Variation
 	public		$variationBarcodes;
 	public		$variationBundleComponents;
 	public		$variationProperties;
+	public		$properties;
 	public		$variationSalesPrices;
 	public		$marketItemNumbers;
 	public		$variationCategories;
