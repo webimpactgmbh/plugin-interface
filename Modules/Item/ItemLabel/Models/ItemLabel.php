5036e29e16adffc67514483b2de35d3a28303961
BuildMonkey <buildmonkey@plentymarkets.com>
2018-07-11 10:25:01 +0200

    Automated interface update 2018-07-11_10:25:01

---
+++ Modules/Item/ItemLabel/Models/ItemLabel.php
@@ -0,0 +1,28 @@
+<?php
+namespace Plenty\Modules\Item\ItemLabel\Models;
+
+
+/**
+ * The item label model
+ */
+abstract class ItemLabel 
+{
+
+	const ITEMS_PER_PAGE = 100;
+
+	const CREATED_AT = 'createdAt';
+
+	const UPDATED_AT = 'updatedAt';
+	public		$labelId;
+	public		$itemId;
+	
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
