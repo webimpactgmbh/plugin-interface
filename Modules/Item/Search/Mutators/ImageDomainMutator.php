d5df574087cf872615877a53563913e44b681d42
BuildMonkey <buildmonkey@plentymarkets.com>
2018-07-13 08:53:31 +0200

    Automated interface update 2018-07-13_08:53:31

--- Modules/Item/Search/Mutators/ImageDomainMutator.php
+++ Modules/Item/Search/Mutators/ImageDomainMutator.php
@@ -4,6 +4,7 @@ namespace Plenty\Modules\Item\Search\Mutators;
 use Plenty\Legacy\Services\Item\Image\ImageService;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\Mutator\MutatorInterface;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\Mutator\ObjectMutator;
+use Plenty\Modules\Helper\Contracts\KeyValueStorageRepositoryContract;
 use Plenty\Modules\System\Contracts\WebstoreConfigurationRepositoryContract;
 use Plenty\Modules\System\Models\WebstoreConfiguration;
 
