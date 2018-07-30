ea250d0cc5c144cffa38b27279b659c78db2cb3a
BuildMonkey <buildmonkey@plentymarkets.com>
2018-07-30 17:22:16 +0200

    Automated interface update 2018-07-30_17:22:16

--- Modules/Item/Variation/Contracts/VariationExportServiceContract.php
+++ Modules/Item/Variation/Contracts/VariationExportServiceContract.php
@@ -9,6 +9,32 @@ use Plenty\Modules\Item\Variation\Services\ExportPreloadValue\ExportPreloadValue
 interface VariationExportServiceContract 
 {
 
+	const SUPPLIER = 'VariationSupplier';
+
+	const STOCK = 'VariationStock';
+
+	const WAREHOUSE = 'VariationWarehouse';
+
+	const BARCODE = 'VariationBarcode';
+
+	const STOCK_STORAGE_LOCATION = 'StockStorageLocation';
+
+	const TAG = 'ItemTag';
+
+	const MARKET_IDENT_NUMBER = 'VariationMarketIdentNumber';
+
+	const COMMENT = 'VariationComment';
+
+	const SALES_PRICE = 'VariationSalesPrice';
+
+	const CATEGORY = 'VariationCategory';
+
+	const PROPERTY = 'VariationEigenschaften';
+
+	const CROSS_SELLING = 'ItemCrossSelling';
+
+	const SHIPPING_PROFILE = 'ItemShippingProfile';
+
 	public function addPreloadTypes(
 		array $types
 	);
