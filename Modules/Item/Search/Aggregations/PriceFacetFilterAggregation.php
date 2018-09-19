0d634f31e0468cb70a89d19e829616698948e3ba
BuildMonkey <buildmonkey@plentymarkets.com>
2018-09-19 11:46:02 +0200

    Automated interface update 2018-09-19_11:46:02

---
+++ Modules/Item/Search/Aggregations/PriceFacetFilterAggregation.php
@@ -0,0 +1,55 @@
+<?php
+namespace Plenty\Modules\Item\Search\Aggregations;
+
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Type\TypeInterface;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Search\Aggregation\AggregationInterface;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Search\Aggregation\Object\ObjectFilterAggregation;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\SourceInterface;
+
+/**
+ * To be written
+ */
+abstract class PriceFacetFilterAggregation implements AggregationInterface
+
+{
+
+	const NAME = 'price-facet';
+
+	abstract public function setFacetId(
+		int $facetId
+	);
+
+	abstract public function getName(
+	):string;
+
+	abstract public function getAggregation(
+	):array;
+
+	abstract public function addFilter(
+		TypeInterface $filter
+	);
+
+	abstract public function toArray(
+	):array;
+
+	abstract public function process(
+		array $data
+	):array;
+
+	abstract public function addAggregation(
+		AggregationInterface $aggregation
+	);
+
+	abstract public function addSource(
+		SourceInterface $source
+	):AggregationInterface;
+
+	abstract public function setPage(
+		int $page, 
+		int $rowsPerPage
+	):AggregationInterface;
+
+	abstract public function getSources(
+	);
+
+}
