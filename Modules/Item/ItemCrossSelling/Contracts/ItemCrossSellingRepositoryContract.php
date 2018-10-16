03038cafb0fdf44d29879579956a0e1df2d4defc
BuildMonkey <buildmonkey@plentymarkets.com>
2018-10-16 10:11:17 +0200

    Automated interface update 2018-10-16_10:11:16

--- Modules/Item/ItemCrossSelling/Contracts/ItemCrossSellingRepositoryContract.php
+++ Modules/Item/ItemCrossSelling/Contracts/ItemCrossSellingRepositoryContract.php
@@ -3,6 +3,10 @@ namespace Plenty\Modules\Item\ItemCrossSelling\Contracts;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Plenty\Modules\Item\ItemCrossSelling\Models\ItemCrossSelling;
+use Plenty\Repositories\Contracts\FilterableContract;
+use Plenty\Repositories\Criteria\Contracts\CriteriableContract;
+use Plenty\Repositories\Criteria\Criteria;
+use Plenty\Repositories\Models\PaginatedResult;
 
 /**
  * Repository for item cross-selling
@@ -33,5 +37,47 @@ interface ItemCrossSellingRepositoryContract
 	public function findAllByItemId(
 		int $itemId
 	):array;
+
+	public function search(
+		int $itemsPerPage = 50, 
+		int $page = 1
+	):PaginatedResult;
+
+	/**
+	 * Resets all Criteria filters by creating a new instance of the builder object.
+	 */
+	public function clearCriteria(
+	);
+
+	/**
+	 * Applies criteria classes to the current repository.
+	 */
+	public function applyCriteriaFromFilters(
+	);
+
+	/**
+	 * Sets the filter array.
+	 */
+	public function setFilters(
+		array $filters = []
+	);
+
+	/**
+	 * Returns the filter array.
+	 */
+	public function getFilters(
+	);
+
+	/**
+	 * Returns a collection of parsed filters as Condition object
+	 */
+	public function getConditions(
+	);
+
+	/**
+	 * Clears the filter array.
+	 */
+	public function clearFilters(
+	);
 
 }
