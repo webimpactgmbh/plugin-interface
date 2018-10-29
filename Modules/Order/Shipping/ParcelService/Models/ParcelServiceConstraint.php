c0767003223a5d6b1db15c919ad0dd89c31e80b8
BuildMonkey <buildmonkey@plentymarkets.com>
2018-10-29 13:01:02 +0100

    Automated interface update 2018-10-29_13:01:02

---
+++ Modules/Order/Shipping/ParcelService/Models/ParcelServiceConstraint.php
@@ -0,0 +1,30 @@
+<?php
+namespace Plenty\Modules\Order\Shipping\ParcelService\Models;
+
+
+/**
+ * The Parcel Service Constraint Model
+ */
+abstract class ParcelServiceConstraint 
+{
+
+	const CREATED_AT = 'createdAt';
+
+	const UPDATED_AT = 'updatedAt';
+	public		$;
+	public		$;
+	public		$;
+	public		$;
+	public		$cost;
+	public		$;
+	
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
