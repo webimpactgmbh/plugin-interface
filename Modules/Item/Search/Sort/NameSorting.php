a8510ed87050ce656f497b415c8e47515a155b5d
BuildMonkey <buildmonkey@plentymarkets.com>
2018-11-05 12:02:29 +0100

    Automated interface update 2018-11-05_12:02:29

--- Modules/Item/Search/Sort/NameSorting.php
+++ Modules/Item/Search/Sort/NameSorting.php
@@ -1,6 +1,7 @@
 <?php
 namespace Plenty\Modules\Item\Search\Sort;
 
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Data\Update\Handler\Traits\MultilingualTrait;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Sorting\BaseSorting;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Sorting\SortingInterface;
 
@@ -13,5 +14,18 @@ abstract class NameSorting implements SortingInterface
 
 	abstract public function toArray(
 	):array;
+
+	abstract public static function isLanguageSupported(
+		string $lang
+	):bool;
+
+	abstract public static function isLanguageActivated(
+		string $lang
+	):bool;
+
+	abstract public static function getM10lByLanguage(
+		string $lang, 
+		string $fallback = "en"
+	):string;
 
 }
