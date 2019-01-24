c53256e859dfd086917ab6ec181580abeb8b8176
BuildMonkey <buildmonkey@plentymarkets.com>
2019-01-24 10:19:03 +0100

    Automated interface update 2019-01-24_10:19:03

--- Modules/Amazon/Contracts/AmazonFulfillmentCenterRepositoryContract.php
+++ Modules/Amazon/Contracts/AmazonFulfillmentCenterRepositoryContract.php
@@ -9,14 +9,29 @@ use Plenty\Modules\Account\Address\Models\Address;
 interface AmazonFulfillmentCenterRepositoryContract 
 {
 
+	/**
+	 * Lists amazon fulfilment centers delivery addresses.
+	 */
+	public function getDeliveryAddresses(
+	):array;
+
+	/**
+	 * Get the delivery address by fulfillment center ID.
+	 */
 	public function getDeliveryAddressByFulfillmentCenterId(
 		string $amazonFulfillmentCenterId
 	):Address;
 
+	/**
+	 * Get the delivery address by GLN.
+	 */
 	public function getDeliveryAddressByGLN(
 		string $GLN
 	):Address;
 
+	/**
+	 * Get the invoice address by GLN.
+	 */
 	public function getInvoiceAddressByGLN(
 		string $GLN
 	):Address;
