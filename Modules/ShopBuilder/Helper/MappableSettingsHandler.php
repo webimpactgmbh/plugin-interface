f95c7bf05848a963327de8d4a7191c93c46e02bb
BuildMonkey <buildmonkey@plentymarkets.com>
2019-02-05 21:27:28 +0100

    Automated interface update 2019-02-05_21:27:28

---
+++ Modules/ShopBuilder/Helper/MappableSettingsHandler.php
@@ -0,0 +1,16 @@
+<?php
+namespace Plenty\Modules\ShopBuilder\Helper;
+
+use Plenty\Modules\Plugin\Contracts\ConfigurationRepositoryContract;
+use Plenty\Modules\Plugin\Contracts\PluginRepositoryContract;
+use Plenty\Modules\Plugin\Models\Configuration;
+use Plenty\Modules\Plugin\Models\Plugin;
+use Plenty\Modules\ShopBuilder\Contracts\GlobalSettingsHandler;
+
+/**
+ * Helper to map global configurations to plugin configs.
+ */
+abstract class MappableSettingsHandler 
+{
+
+}
