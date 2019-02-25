571af2ba3e481730784ec08c2f0e39d31572911b
BuildMonkey <buildmonkey@plentymarkets.com>
2019-02-25 10:59:54 +0100

    Automated interface update 2019-02-25_10:59:54

--- Modules/Item/Search/Mutators/ImageMutator.php
+++ Modules/Item/Search/Mutators/ImageMutator.php
@@ -11,8 +11,16 @@ abstract class ImageMutator implements MutatorInterface
 
 {
 
+	const SORT_ID = 'id';
+
+	const SORT_POSITION = 'position';
+
 	abstract public function getPath(
 	):string;
+
+	abstract public function setSorting(
+		string $field
+	);
 
 	abstract public function mutateObject(
 		array $data
