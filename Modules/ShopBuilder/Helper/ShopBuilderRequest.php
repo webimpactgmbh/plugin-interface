007ec3d9dd2015e3469875a09b1416765fc0f384
BuildMonkey <buildmonkey@plentymarkets.com>
2019-03-04 15:42:00 +0100

    Automated interface update 2019-03-04_15:42:00

--- Modules/ShopBuilder/Helper/ShopBuilderRequest.php
+++ Modules/ShopBuilder/Helper/ShopBuilderRequest.php
@@ -2,7 +2,7 @@
 namespace Plenty\Modules\ShopBuilder\Helper;
 
 use Illuminate\Http\Request;
-use Plenty\Plugin\Application;
+use Plenty\Modules\Authentication\Services\Hasher\AdminPreviewHasher;
 
 /**
  * Information provided by each request made from the shop builder preview.
