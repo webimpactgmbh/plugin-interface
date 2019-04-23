ebef0b112474ee71367da9bb38f814afad9353c7
BuildMonkey <buildmonkey@plentymarkets.com>
2019-04-23 15:52:07 +0200

    Automated interface update 2019-04-23_15:52:07

--- Modules/Plugin/Contracts/PluginRepositoryContract.php
+++ Modules/Plugin/Contracts/PluginRepositoryContract.php
@@ -21,4 +21,24 @@ interface PluginRepositoryContract
 		int $itemsPerPage = \Plenty\Modules\Plugin\Models\Plugin::DEFAULT_ITEMS_PER_PAGE
 	):PaginatedResult;
 
+	public function isActiveInPluginSet(
+		int $pluginId, 
+		int $pluginSetId
+	):bool;
+
+	public function isActiveInPluginSetByName(
+		string $pluginName, 
+		int $pluginSetId
+	):bool;
+
+	public function isActiveInWebstore(
+		int $pluginId, 
+		int $webstoreId
+	):bool;
+
+	public function isActiveInWebstoreByPluginName(
+		string $pluginName, 
+		int $webstoreId
+	):bool;
+
 }
