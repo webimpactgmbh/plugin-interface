691c4004fdbd08238348e7fd2c4170801c8bdc57
BuildMonkey <buildmonkey@plentymarkets.com>
2019-05-28 13:53:35 +0200

    Automated interface update 2019-05-28_13:53:35

---
+++ Modules/ShopBuilder/Providers/DataFieldProvider.php
@@ -0,0 +1,39 @@
+<?php
+namespace Plenty\Modules\ShopBuilder\Providers;
+
+use Plenty\Modules\ShopBuilder\Contracts\DataFieldRepositoryContract;
+use Plenty\Plugin\Translation\Translator;
+
+/**
+ * Base class for data field providers.
+ */
+abstract class DataFieldProvider 
+{
+
+	abstract public function register(
+	);
+
+	/**
+	 * Register a new data field.
+	 */
+	public function addField(
+		string $label, 
+		string $expression
+	)
+	{
+		return null;
+	}
+
+	/**
+	 * Register a nested provider containing a list of child data fields.
+	 */
+	public function addChildProvider(
+		string $label, 
+		string $childProviderClass, 
+		array $params = []
+	)
+	{
+		return null;
+	}
+
+}
