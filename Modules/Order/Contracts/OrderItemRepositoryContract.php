29cecaafce3ff1db419a999e6746eac60ffced70
BuildMonkey <buildmonkey@plentymarkets.com>
2019-06-13 08:58:11 +0200

    Automated interface update 2019-06-13_08:58:11

--- Modules/Order/Contracts/OrderItemRepositoryContract.php
+++ Modules/Order/Contracts/OrderItemRepositoryContract.php
@@ -6,6 +6,9 @@ use Plenty\Modules\Order\Exceptions\OrderItemDeleteException;
 use Plenty\Modules\Order\Exceptions\OrderItemRelationSaveException;
 use Plenty\Modules\Order\Exceptions\OrderItemSaveException;
 use Plenty\Modules\Order\Models\OrderItem;
+use Plenty\Repositories\Contracts\FilterableContract;
+use Plenty\Repositories\Criteria\Contracts\CriteriableContract;
+use Plenty\Repositories\Criteria\Criteria;
 use Plenty\Repositories\Models\PaginatedResult;
 
 /**
@@ -38,5 +41,42 @@ interface OrderItemRepositoryContract
 		int $itemsPerPage = 50, 
 		array $with = []
 	):PaginatedResult;
+
+	/**
+	 * Sets the filter array.
+	 */
+	public function setFilters(
+		array $filters = []
+	);
+
+	/**
+	 * Returns the filter array.
+	 */
+	public function getFilters(
+	);
+
+	/**
+	 * Returns a collection of parsed filters as Condition object
+	 */
+	public function getConditions(
+	);
+
+	/**
+	 * Clears the filter array.
+	 */
+	public function clearFilters(
+	);
+
+	/**
+	 * Resets all Criteria filters by creating a new instance of the builder object.
+	 */
+	public function clearCriteria(
+	);
+
+	/**
+	 * Applies criteria classes to the current repository.
+	 */
+	public function applyCriteriaFromFilters(
+	);
 
 }
