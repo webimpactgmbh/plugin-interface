e67bde5ddc6bb944afb3bc228e5934e294b56ea1
BuildMonkey <buildmonkey@plentymarkets.com>
2019-07-10 09:57:01 +0200

    Automated interface update 2019-07-10_09:57:01

---
+++ Modules/Account/Contracts/IaMnOtReAl.php
@@ -0,0 +1,16 @@
+<?php
+namespace Plenty\Modules\Account\Contracts;
+
+
+/**
+ * IaMnOtReAl is not a real thing. It is temporarily needed to fix some issues with the interface integrity check and will be removed shortly after.
+ */
+interface IaMnOtReAl 
+{
+
+	public function nOtAReAlMeThOd(
+		string $foo_string, 
+		int $bar_int
+	):bool;
+
+}
