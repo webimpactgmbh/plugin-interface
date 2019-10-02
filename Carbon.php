055d2b5b4071f196550f09e492827f0d9455cf11
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2019-10-02 06:44:46 +0200

    Automated interface update 2019-10-02_06:44:46

--- Carbon.php
+++ Carbon.php
@@ -25,4 +25,59 @@ class Carbon extends \DateTime
 	public		$timezone;
 	public		$tz;
 	
+	/**
+	 * Get a Carbon instance for the current date and time.
+	 */
+	abstract public static function now(
+		 $tz = null
+	):Carbon;
+
+	/**
+	 * Get the difference in years
+	 */
+	abstract public function diffInYears(
+		 $date = null, 
+		bool $absolute = true
+	):int;
+
+	/**
+	 * Get the difference in months
+	 */
+	abstract public function diffInMonths(
+		 $date = null, 
+		bool $absolute = true
+	):int;
+
+	/**
+	 * Get the difference in weeks
+	 */
+	abstract public function diffInWeeks(
+		 $date = null, 
+		bool $absolute = true
+	):int;
+
+	/**
+	 * Get the difference in days
+	 */
+	abstract public function diffInDays(
+		 $date = null, 
+		bool $absolute = true
+	):int;
+
+	/**
+	 * Get the difference in hours.
+	 */
+	abstract public function diffInHours(
+		 $date = null, 
+		bool $absolute = true
+	):int;
+
+	/**
+	 * Get the difference in minutes.
+	 */
+	abstract public function diffInMinutes(
+		 $date = null, 
+		bool $absolute = true
+	):int;
+
 }
