a556df559e9ec24f95c17b59aafcc88339630d30
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2019-10-14 10:17:27 +0200

    Automated interface update 2019-10-14_10:17:27

---
+++ Modules/Pim/SearchService/Aggregations/Price/PriceRangeAggregation.php
@@ -0,0 +1,50 @@
+<?php
+namespace Plenty\Modules\Pim\SearchService\Aggregations\Price;
+
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Search\Aggregation\AggregationInterface;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Search\Aggregation\Object\ObjectAggregation;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\SourceInterface;
+
+/**
+ * To be written
+ */
+abstract class PriceRangeAggregation implements AggregationInterface
+
+{
+
+	const NAME = 'priceRange';
+
+	abstract public function getName(
+	);
+
+	abstract public function setPriceIds(
+		array $ids
+	);
+
+	abstract public function process(
+		 $data
+	);
+
+	abstract public function getAggregation(
+	);
+
+	abstract public function toArray(
+	):array;
+
+	abstract public function addAggregation(
+		AggregationInterface $aggregation
+	);
+
+	abstract public function addSource(
+		SourceInterface $source
+	):AggregationInterface;
+
+	abstract public function setPage(
+		int $page, 
+		int $rowsPerPage
+	):AggregationInterface;
+
+	abstract public function getSources(
+	);
+
+}
