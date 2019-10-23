af11f7bb1c304e62a2ae9f05e7dfa19cbade0151
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2019-10-23 08:46:37 +0200

    Automated interface update 2019-10-23_08:46:37

---
+++ Modules/Webshop/Consent/Contracts/ConsentRepositoryContract.php
@@ -0,0 +1,58 @@
+<?php
+namespace Plenty\Modules\Webshop\Consent\Contracts;
+
+use Illuminate\Support\Collection;
+use Plenty\Modules\Webshop\Consent\Models\ConsentGroup;
+
+/**
+ * Repository to register consents and check for users response.
+ */
+interface ConsentRepositoryContract 
+{
+
+	/**
+	 * Register a new group of consents.
+	 */
+	public function registerConsentGroup(
+		string $key, 
+		string $label, 
+		array $options = []
+	);
+
+	/**
+	 * Register a new consent.
+	 */
+	public function registerConsent(
+		string $key, 
+		string $label, 
+		array $options = []
+	);
+
+	/**
+	 * Get all consent groups including all consents.
+	 */
+	public function getConsentGroups(
+	):Collection;
+
+	/**
+	 * Check if the user has already responded the consents.
+	 */
+	public function hasResponse(
+	):bool;
+
+	/**
+	 * Set the response for a consent or a group of consents.
+	 */
+	public function setResponse(
+		string $key, 
+		bool $isConsented = false
+	);
+
+	/**
+	 * Check if the user has consented.
+	 */
+	public function isConsented(
+		string $key
+	):bool;
+
+}
