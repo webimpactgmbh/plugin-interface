a7a3ab06cff9373e78305178146467a58685e619
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2019-11-08 11:03:45 +0100

    Automated interface update 2019-11-08_11:03:45

---
+++ Modules/Fulfillment/Services/FulfillmentReturnService.php
@@ -0,0 +1,26 @@
+<?php
+namespace Plenty\Modules\Fulfillment\Services;
+
+use Plenty\Legacy\Repositories\Fulfillment\Shipping\LegacyFulfillmentShippingRepository;
+use Plenty\Modules\Comment\Contracts\CommentRepositoryContract;
+use Plenty\Modules\Order\Contracts\OrderRepositoryContract;
+use Plenty\Modules\Order\Models\Order;
+use Plenty\Modules\Order\Shipping\Contracts\ParcelServicePresetRepositoryContract;
+use Plenty\Modules\Order\Shipping\ServiceProvider\Contracts\ShippingServiceProviderRepositoryContract;
+use Plenty\Modules\Plugin\Contracts\PluginRepositoryContract;
+use Plenty\Modules\User\Contracts\UserRepositoryContract;
+
+/**
+ * The FulfillmentReturnService is the service for return shipments of an order.
+ */
+abstract class FulfillmentReturnService 
+{
+
+	/**
+	 * Register the return of an order.
+	 */
+	abstract public function registerReturns(
+		int $orderId
+	):bool;
+
+}
