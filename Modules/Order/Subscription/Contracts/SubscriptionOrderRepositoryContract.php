63d5422f025c4772a7da4f81766ba2db3506951e
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2019-11-19 11:40:01 +0100

    Automated interface update 2019-11-19_11:40:01

--- Modules/Order/Subscription/Contracts/SubscriptionOrderRepositoryContract.php
+++ Modules/Order/Subscription/Contracts/SubscriptionOrderRepositoryContract.php
@@ -1,6 +1,7 @@
 <?php
 namespace Plenty\Modules\Order\Subscription\Contracts;
 
+use Plenty\Modules\Order\Models\Order;
 
 /**
  * The SubscriptionOrderRepositoryContract allows you to find subscriptions, that due either today or on any given date, so that corresponding orders are created for them.
@@ -18,7 +19,15 @@ interface SubscriptionOrderRepositoryContract
 	 * Finds all subscriptions that are due on the given date and creates corresponding orders for them.
 	 */
 	public function runAnyDate(
-		string $date
+		string $runDate
 	):bool;
+
+	/**
+	 * Create an order for today for a given subscription.
+	 */
+	public function manualRun(
+		int $orderId, 
+		bool $skipNextRun = false
+	):Order;
 
 }
