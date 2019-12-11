d09679f4082e8871a21b087431c3f897aef187f5
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2019-12-11 10:24:34 +0100

    Automated interface update 2019-12-11_10:24:34

---
+++ Modules/Pim/SearchService/Aggregations/Facet/NestedFacetValuesAggregation.php
@@ -0,0 +1,47 @@
+<?php
+namespace Plenty\Modules\Pim\SearchService\Aggregations\Facet;
+
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Processor\ProcessorInterface;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Search\Aggregation\AggregationInterface;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Search\Aggregation\Object\ObjectAggregation;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\SourceInterface;
+
+/**
+ * To be written
+ */
+abstract class NestedFacetValuesAggregation implements AggregationInterface
+
+{
+
+	const NAME = 'nestedFacetValues';
+
+	abstract public function getName(
+	);
+
+	abstract public function getAggregation(
+	);
+
+	abstract public function toArray(
+	):array;
+
+	abstract public function process(
+		array $data
+	):array;
+
+	abstract public function addAggregation(
+		AggregationInterface $aggregation
+	);
+
+	abstract public function addSource(
+		SourceInterface $source
+	):AggregationInterface;
+
+	abstract public function setPage(
+		int $page, 
+		int $rowsPerPage
+	):AggregationInterface;
+
+	abstract public function getSources(
+	);
+
+}
