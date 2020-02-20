bdff9e97e987f5b6698d65d4c231cb0677e1c04c
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2020-02-20 11:00:08 +0100

    Automated interface update 2020-02-20_11:00:08

---
+++ Modules/MyView/Contracts/ViewSetRepositoryContract.php
@@ -0,0 +1,47 @@
+<?php
+namespace Plenty\Modules\MyView\Contracts;
+
+
+/**
+ * This interface provides methods to create, get, update and delete ViewSets
+ */
+interface ViewSetRepositoryContract 
+{
+
+	/**
+	 * Search the list of view sets.
+	 */
+	public function search(
+		array $searchParams
+	);
+
+	/**
+	 * Get given view set.
+	 */
+	public function get(
+		int $id
+	);
+
+	/**
+	 * Create given view set.
+	 */
+	public function create(
+		array $data
+	);
+
+	/**
+	 * Update given view set.
+	 */
+	public function update(
+		int $id, 
+		array $data
+	);
+
+	/**
+	 * Delete given view set.
+	 */
+	public function delete(
+		int $id
+	);
+
+}
