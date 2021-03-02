83651fe1d576feb9f04560d8d36e9658fa318688
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2021-03-03 00:02:18 +0100

    Automated interface update 2021-03-03_00:02:18

---
+++ Modules/AmazonFlatfiles/Exceptions/AmazonFlatFileFormatException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Plenty\Modules\AmazonFlatfiles\Exceptions;
+
+
+/**
+ * Class AmazonClientException
+ */
+abstract class AmazonFlatFileFormatException extends \Exception 
+
+{
+
+}
